1ffa378955b0719c0b0fd65c9f9b5754ee8b1f9e
av9248 <av9248@gmail.com>
2024-09-30 09:35:18 -0500

    workingon app

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -1,32 +1,153 @@
 # Import the dependencies.
+import numpy as np
+import pandas as pd
+import datetime as dt
 
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+from sqlalchemy import desc
 
+from flask import Flask, jsonify
 
 #################################################
 # Database Setup
 #################################################
 
 # Create engine using the `hawaii.sqlite` database file
-
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 # Declare a Base using `automap_base()`
-
+Base = automap_base()
 # Use the Base class to reflect the database tables
-
+Base.prepare(engine, reflect=True)
 
 # Assign the measurement class to a variable called `Measurement` and
 # the station class to a variable called `Station`
-
+Measurement = Base.classes.measurement
+Station = Base.classes.station
 
 # Create a session
-
+session = Session(engine)
 
 #################################################
 # Flask Setup
 #################################################
-
+app = Flask(__name__)
 
 
 
 #################################################
 # Flask Routes
 #################################################
+@app.route("/")
+def welcome():
+    """List all available api routes."""
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/start<br/>"
+        f"/api/v1.0/end<br/>"
+    )
+
+
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    # Calculate the date one year from the last date in data set.
+    previous_year = dt.date(2017,8,23) - dt.timedelta(days=365)
+
+    # Perform a query to retrieve the data and precipitation scores
+    precipitation_data = session.query(Measurement.date, Measurement.prcp).filter(Measurement.date >= previous_year).order_by(Measurement.date).all()
+
+    session.close()
+
+    #dictionary using date as the key and prcp as the value
+    precip = {date: prcp for date, prcp in precipitation}
+
+    return jsonify(precip)
+
+
+@app.route("/api/v1.0/stations")
+def station():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    """Return a list of passenger data including the name, age, and sex of each passenger"""
+    # Query stations
+    results = session.query(Station.station).all()
+
+    session.close()
+
+    # Return a JSON list of stations from the dataset
+    stations = list(np.ravel(results))
+
+    return jsonify(stations)
+
+@app.route("/api/v1.0/tobs")
+def tobs():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    # Calculate the date one year from the last date in data set.
+    previous_year = dt.date(2017,8,23) - dt.timedelta(days=365)
+
+    temperature_data = session.query(Measurement.date, Measurement.tobs).filter(Measurement.station == "USC00519281",Measurement.date >= previous_year).all()
+
+
+    session.close()
+
+    # Return a JSON list of stations from the dataset
+    temp = list(np.ravel(temperature_data))
+
+    return jsonify(temp)
+
+@app.route("/api/v1.0/<start>")
+def start():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    # Query 
+    temperature_stats = session.query(
+    func.min(Measurement.tobs).label('min_temp'),
+    func.max(Measurement.tobs).label('max_temp'),
+    func.avg(Measurement.tobs).label('avg_temp')
+    ).filter(Measurement.station == "USC00519281").first()
+
+    session.close()
+
+    # Return a JSON list of stations from the dataset
+    stats = list(np.ravel(temperature_stats))
+
+    return jsonify(stats)
+
+@app.route("/api/v1.0/<start>/<end>")
+def start_end(start, end):
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+
+    # The format "%m%d%Y" is the standard format for the date in the dataset.
+    start = dt.datetime.strptime(start, "%m%d%Y")
+    end = dt.datetime.strptime(end, "%m%d%Y")
+
+    results = session.query(
+    func.min(Measurement.tobs).label('min_temp'),
+    func.max(Measurement.tobs).label('max_temp'),
+    func.avg(Measurement.tobs).label('avg_temp')
+    ).filter(Measurement.date >= start).\
+    filter(Measurement.date <= end).all() == "USC00519281".first()
+
+    session.close()
+
+    # Return a JSON list of stations from the dataset
+    stats = list(np.ravel(results))
+
+    return jsonify(stats)
+
+if __name__ == '__main__':
+    app.run(debug=True)
